1fd3003fc5a40a824d0d17a603cb50706b713894
KeyllaFibriA <keyllafbri@gmail.com>
2023-12-06 14:13:16 +0700

    first commit

---
+++ src/simpleant/perkalian.java
@@ -0,0 +1,7 @@
+package simpleant; 
+public class perkalian { 
+    public static void main(String[] args) { 
+        int  hasil = 20 * 150 ;
+        System.out.println("Maka hasil perkalian dari 20 * 150 adalah " + hasil);
+    }
+}
